2bd280704a8387de9996c00fbab4008f26189597
Damien Guard <damieng@gmail.com>
2013-01-14 13:28:29 -0800

    Add licence and copyright headers.

--- Source/CSharpAnalytics/CustomVariables/FinalCustomVariables.cs
+++ Source/CSharpAnalytics/CustomVariables/FinalCustomVariables.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿﻿// Copyright (c) Attack Pattern LLC.  All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
--- Source/CSharpAnalytics/CustomVariables/ICustomVariable.cs
+++ Source/CSharpAnalytics/CustomVariables/ICustomVariable.cs
@@ -1,4 +1,7 @@
-﻿namespace CSharpAnalytics.CustomVariables
+﻿﻿// Copyright (c) Attack Pattern LLC.  All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+namespace CSharpAnalytics.CustomVariables
 {
     /// <summary>
     /// Interface for classes that can provide a name and value for custom variables.
