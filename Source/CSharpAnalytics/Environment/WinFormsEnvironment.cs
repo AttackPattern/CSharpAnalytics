e037f804a7b049eebdb9e0c6ef06987578c158aa
Damien Guard <damien@envytech.co.uk>
2014-04-21 23:43:25 -0700

    Implement CharacterSet, ScreenColorDepth and LanguageCode for WinForms applications.

--- Source/CSharpAnalytics/Environment/WinFormsEnvironment.cs
+++ Source/CSharpAnalytics/Environment/WinFormsEnvironment.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Forms;
+﻿﻿// Copyright (c) Attack Pattern LLC.  All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+using System.Globalization;
+using System.Text;
+using System.Windows.Forms;
 
 namespace CSharpAnalytics.Environment
 {
@@ -8,17 +14,20 @@ namespace CSharpAnalytics.Environment
     /// </summary>
     internal class WinFormsEnvironment : IEnvironment
     {
-        public string CharacterSet { get { return "UTF-8"; } }
+        public string CharacterSet
+        {
+            get { return Encoding.Default.BodyName; }
+        }
 
         public string LanguageCode
         {
-            get { return "en-US"; }
+            get { return CultureInfo.CurrentUICulture.Name; }
         }
 
         public string FlashVersion { get { return null; } }
         public bool? JavaEnabled { get { return null; } }
 
-        public uint ScreenColorDepth { get { return 32; } }
+        public uint ScreenColorDepth { get { return (uint)Screen.PrimaryScreen.BitsPerPixel; } }
 
         public uint ScreenHeight
         {
