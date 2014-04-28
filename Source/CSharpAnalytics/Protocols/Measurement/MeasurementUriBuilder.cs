27401113a68ae50808e8e31f15cfa0b616cc8041
Damien Guard <damien@envytech.co.uk>
2014-04-28 14:44:11 -0700

    Only send Java Enabled "je" flag if we have a value.

--- Source/CSharpAnalytics/Protocols/Measurement/MeasurementUriBuilder.cs
+++ Source/CSharpAnalytics/Protocols/Measurement/MeasurementUriBuilder.cs
@@ -129,7 +129,8 @@ namespace CSharpAnalytics.Protocols.Measurement
             if (!String.IsNullOrWhiteSpace(environment.FlashVersion))
                 yield return KeyValuePair.Create("fl", environment.FlashVersion);
 
-            yield return KeyValuePair.Create("je", environment.JavaEnabled == true ? "1" : "0");
+            if (environment.JavaEnabled.HasValue)
+                yield return KeyValuePair.Create("je", environment.JavaEnabled.Value ? "1" : "0");
 
             if (environment.ScreenColorDepth > 0)
                 yield return KeyValuePair.Create("sd", String.Format("{0}-bit", environment.ScreenColorDepth));
