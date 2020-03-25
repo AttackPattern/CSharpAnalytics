89b1a5b62be6d171d1aa5fcf0208feeb239fd476
Alex Tamayo <alex.tamayo@sonova.com>
2020-03-25 15:32:00 +0100

    Tests converted to sdk style format.
    (cherry picked from commit 5788ec7e1b7d3eb23a63a5abce36da2ae3dd3aa0)

--- src/CSharpAnalytics/Properties/AssemblyInfo.cs
+++ src/CSharpAnalytics/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Net45")]
+[assembly: InternalsVisibleTo("CSharpAnalytics.Tests")]
 [assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows8")]
 [assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows81")]
 [assembly: InternalsVisibleTo("CSharpAnalytics.Test.WindowsPhone8")]
