e4719b5635fcc453096d8dd9b574cfa36c9fd116
Alex Tamayo <alex.tamayo@sonova.com>
2020-03-25 14:44:20 +0100

    Added CSharpAnalytics repository to sonova git
    (cherry picked from commit 2067e9ae613f969f9647f84748e27bde371c656d)

---
+++ src/CSharpAnalytics/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Net45")]
+[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows8")]
+[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows81")]
+[assembly: InternalsVisibleTo("CSharpAnalytics.Test.WindowsPhone8")]
