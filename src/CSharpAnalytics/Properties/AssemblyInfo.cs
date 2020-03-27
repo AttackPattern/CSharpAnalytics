74a1487c4fbd105c3b04133e43d556fe3947a56a
Alex Tamayo <alex.tamayo@sonova.com>
2020-03-27 13:26:47 +0100

    Updated readme, added info how to pack.

--- src/CSharpAnalytics/Properties/AssemblyInfo.cs
+++ src/CSharpAnalytics/Properties/AssemblyInfo.cs
@@ -4,6 +4,3 @@ using System.Runtime.InteropServices;
 
 
 [assembly: InternalsVisibleTo("CSharpAnalytics.Tests")]
-[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows8")]
-[assembly: InternalsVisibleTo("CSharpAnalytics.Test.Windows81")]
-[assembly: InternalsVisibleTo("CSharpAnalytics.Test.WindowsPhone8")]
